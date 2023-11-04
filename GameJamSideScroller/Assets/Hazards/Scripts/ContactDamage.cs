c11d08564bedb649ffb3314f1d2545b018ac15ff
Vane Louhelainen <mcturbo.vjml@gmail.com>
2023-11-04 16:14:10 +0200

    Contact Damage
    • Made PlayerHealth TakeDamage method public
    • Added knockback and damage implementation to ContactDamage class

--- GameJamSideScroller/Assets/Hazards/Scripts/ContactDamage.cs
+++ GameJamSideScroller/Assets/Hazards/Scripts/ContactDamage.cs
@@ -11,6 +11,12 @@ namespace Hazard
     {
         public int damage = 1;
         public float knockback = 1f;
+        public float cooldown = 1f;
+        private float cdTimer = 0f;
+        private Vector2 knockbackDirection;
+        
+        /*[SerializeField] private PlayerHealth health;
+        [SerializeField] private PlayerMovement move;*/
 
         // Start is called before the first frame update
         void Start()
@@ -20,13 +26,41 @@ namespace Hazard
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            // TODO: trigger methods to damage and knockback(?) player
+            // if colliding thing is player
+            if (collision.gameObject.name == "Player")
+            {
+                if (cdTimer > 0f)
+                {
+                    Debug.Log("Contact damage for " + gameObject.name + " on cooldown");
+                }
+                else
+                {
+                    Debug.Log("Hit player");
+                    collision.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
+
+                    if (collision.gameObject.transform.position.x < transform.position.x)
+                    {
+                        knockbackDirection = new Vector2(-2, 1).normalized;
+                    }
+                    else
+                    {
+                        knockbackDirection = new Vector2(2, 1).normalized;
+                    }
+
+                    collision.gameObject.GetComponent<Rigidbody2D>().AddForce(knockbackDirection * knockback, ForceMode2D.Impulse);
+
+                    cdTimer = cooldown;
+                }
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            if (cdTimer > 0f)
+            {
+                cdTimer -= Time.deltaTime;
+            }
         }
     }
 
