871293666eca2dbd36b3e5849d7cb154462d97c5
Vane Louhelainen <mcturbo.vjml@gmail.com>
2023-11-04 13:07:03 +0200

    Simple darkness
    • Renamed SampleScene to HazardTestScene (hopefully to prevent merge conflicts)
    • Added Light class that does nothing by itself as of now
    • Added RemovableDarkness class that gets deleted when in contact with a Light object

---
+++ GameJamSideScroller/Assets/Hazards/Scripts/RemovableDarkness.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hazard
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(ContactDamage))]
+    public class RemovableDarkness : MonoBehaviour
+    {
+        private BoxCollider2D coll;
+        // Start is called before the first frame update
+        void Start()
+        {
+            coll = GetComponent<BoxCollider2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.GetComponent<Light>() != null)
+            {
+                Fade();
+            }
+        }
+
+        private void Fade()
+        {
+            Destroy(gameObject, 0f);
+        }
+    }
+
+}
