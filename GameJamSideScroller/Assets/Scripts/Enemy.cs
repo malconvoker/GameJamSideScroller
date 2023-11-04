05d88971e9ea8a5edaae453d48112bf248bd7d5b
Vane Louhelainen <mcturbo.vjml@gmail.com>
2023-11-04 10:53:48 +0200

    Fixed project
    • Moved Unity project to a subfolder
    • Recreated the whole thing to fix Visual Studio integration

---
+++ GameJamSideScroller/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class for enemies, hazards that can move and be killed
+/// </summary>
+public class Enemy : MonoBehaviour
+{
+    public int hp = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
